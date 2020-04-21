b414ffecd38437c0fdec95925e4917961aac6844
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-04-21 12:59:12 +0530

    Initial commit

---
+++ app/src/main/java/com/AnmolSinha/drum/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.AnmolSinha.drum;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity
+{
+    private static int SPLASH_SCREEN = 3000;
+    Animation topAnim, bottomAnim;
+    TextView textView;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        imageView=findViewById(R.id.imageView);
+        textView=findViewById(R.id.textView);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        imageView.setAnimation(topAnim);
+        textView.setAnimation(bottomAnim);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
